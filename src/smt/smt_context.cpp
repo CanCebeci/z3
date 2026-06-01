fc0b69efb5c77cb81ace3b421967f279bf4f6109
Can Cebeci <t-cancebeci@microsoft.com>
2026-06-01 16:41:28 -0700

    Fix corner case with constants

--- src/smt/smt_context.cpp
+++ src/smt/smt_context.cpp
@@ -106,6 +106,7 @@ namespace smt {
         m_unknown("unknown"),
         m_unsat_core(m),
         m_cgr_on_failure_todo(m_cgr_on_failure_sm),
+        m_cgr_listeners(m),
         m_mk_bool_var_trail(*this),
         m_mk_enode_trail(*this),
         m_mk_lambda_trail(*this),
@@ -4904,12 +4905,14 @@ namespace smt {
 
     enode* context::find_enode_rec(expr* e) {
         if (enode* n = find_enode(e)) {
-            std:: cout << "Found enode for " << mk_pp(e, m) << ": #" << n->get_cg()->get_owner_id() << std::endl;
+            std:: cout << "Found enode for " << mk_pp(e, m) << ": #" << n->get_owner_id() << std::endl;
             return n;
         }
         if (!is_app(e))
             return nullptr;
         app* a = to_app(e);
+        if (a->get_num_args() == 0)
+            return nullptr;
         enode_vector arg_enodes;
         for (expr* arg : *a)
             if (enode* n = find_enode_rec(arg))
@@ -4918,7 +4921,7 @@ namespace smt {
                 return nullptr;
         // The hash function for cg_table looks up the root enode for each argument, so we don't need to do it here.
         enode* res = get_enode_eq_to(a->get_decl(), a->get_num_args(), arg_enodes.data());
-        std::cout << "Enode_eq_to " << mk_pp(e, m) << ": #" << res->get_cg()->get_owner_id() << std::endl;
+        std::cout << "Enode_eq_to " << mk_pp(e, m) << ": #" << res->get_owner_id() << std::endl;
         return res;
     }
 
@@ -4926,8 +4929,10 @@ namespace smt {
         smt::enode* n = find_enode_rec(e);
         if (!n)
             std::cout << "No enodes congruent to " << mk_pp(e, m) << "\n";
-        else
-            std::cout << "The congruence representative for " << mk_pp(e, m) << " is #" << n->get_cg()->get_owner_id() << ": " << mk_pp(n->get_cg()->get_expr(), m) << "\n";
+        else {
+            enode* cg = n->get_num_args() > 0 ? n->get_cg() : n;  // constants have no cg
+            std::cout << "The congruence representative for " << mk_pp(e, m) << " is #" << cg->get_owner_id() << ": " << mk_pp(cg->get_expr(), m) << "\n";
+        }
     }
 
     expr* context::sexpr_to_expr(sexpr* s) {
